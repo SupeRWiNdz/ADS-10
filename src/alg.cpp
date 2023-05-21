0128240de534b1e81233277edcc53fa4702a393d
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-05-21 20:52:27 +0300

    Фагин 22-ИСТ-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,26 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+unsigned int factorial(unsigned int n)
+{
+    unsigned int result = 1;
+    while (n > 0)
+    {
+        result *= n;
+        n--;
+    }
+    return result;
+}
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    n -= 1;
+    const Tree* ptr = &tree;
+    std::vector<char> result;
+    for (int i = 0; i < tree.children.size(); i++) {
+        int temp = factorial(ptr->children.size() - 1);
+        ptr = ptr->children[n / temp];
+        result.push_back(ptr->ch);
+        n %= temp;
+    }
+    return result;
 }
