64b18b8a16747b97e52011cbbee1b6e6fb818632
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-05-21 20:51:41 +0300

    Фагин 22-ИСТ-1

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,31 @@
 // Copyright 2022 NNTU-CS
+#include <vector>
+#include <iostream>
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+
+class Tree {
+ private:
+    void addChild(Tree *root, std::vector<char> values) {
+        for (int i = 0; i < size(values); i++) {
+            Tree* child = new Tree(values[i]);
+            children.push_back(child);
+            std::vector<char> temp = values;
+            temp.erase(temp.begin() + i);
+            child->addChild(child, temp);
+        }
+    }
+    Tree(char x) {
+        ch = x;
+    }
+ public:
+    Tree(std::vector<char> values)
+    {
+        Tree* root = new Tree('*');
+       addChild(root, values);
+    }
+    char ch = ' ';
+    std::vector<Tree*> children;
+};
 
 #endif  // INCLUDE_TREE_H_
